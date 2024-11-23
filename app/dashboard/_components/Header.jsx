f872487d3d33c291b0fff3204e23d71158e2cf6b
jagpreet13 <singhjagpreet0013@gmail.com>
2024-11-23 13:40:17 +0530

    logo remove from header

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,9 +1,8 @@
 "use client"
 import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 function Header() {
 
@@ -14,7 +13,7 @@ function Header() {
 
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-         <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
+         {/* <Image src={'/logo.svg'} width={160} height={100} alt='logo' /> */}
         <ul className='hidden md:flex gap-6'>
           <Link href={"/dashboard"}>
             <li className={`hover:text-primary hover:font-bold transition-all
